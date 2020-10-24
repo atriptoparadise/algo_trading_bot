0ec8b5849829f0d7ae3667ad4197c3b7180503b1
atriptoparadise <qs2178@columbia.edu>
2020-10-24 15:31:14 -0400

    Added holding_stocks functionality and pickle file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import schedule
 import logging
 import requests
 import json
+from config import *
 
 logfile = 'logs/signal_{}.log'.format(datetime.now().date())
 logging.basicConfig(filename=logfile, level=logging.WARNING)
@@ -17,16 +18,20 @@ class LiveTrade(object):
         self.alpha_volume = alpha_volume
         self.balance = balance
         self.limit_order = balance / volatility
-        self.holding_stocks = []
         self.api = None
+        self.holding_stocks = {}
+        self.request_headers = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+        self.base_url = 'https://paper-api.alpaca.markets'
 
     def setup(self):
-        self.api = tradeapi.REST('PKDLZFEJND51O3Q3TZ3F', 
-                            'xguRqEqr3cw0sNA2cQcxIeCuIacqN9mXnvB8LyaO',
-                            api_version = 'v2')
+        self.api = tradeapi.REST(API_KEY, 
+                                SECRET_KEY, 
+                                api_version = 'v2')
+        data = self.load_data('data')
+        return data, data.keys()
 
-    def load_data(self):
-        with open("data/data.pickle", "rb") as f:
+    def load_data(self, filename):
+        with open(f"data/{filename}.pickle", "rb") as f:
             objects = []
             while True:
                 try:
@@ -34,8 +39,19 @@ class LiveTrade(object):
                 except EOFError:
                     break
         return objects[0]
+        
+    def save_data(self, filename):
+        with open(f"data/{filename}.pickle", 'wb') as f:
+            pickle.dump(self.holding_stocks, f, pickle.HIGHEST_PROTOCOL)
 
     def find_signal(self, ticker, ticker_data):
+        if ticker in self.holding_stocks:
+            current_price = api.get_barset(ticker, '1Min', limit = 1)[ticker][-1].c
+            if current_price <= 0.95 * self.holding_stocks[ticker][0]:
+                self.sell(ticker, share=self.holding_stocks[ticker][1])
+                self.holding_stocks.pop(ticker)
+            return
+
         # solid 15 mins
         print(ticker)
         stock_barset = self.api.get_barset(ticker, '15Min', limit = 27).df.reset_index()
@@ -58,14 +74,14 @@ class LiveTrade(object):
 
         
         current_price = stock_barset_moving.iloc[-1, 2]
-        today_high_so_far = max(stock_barset.iloc[:, 2])
-        today_volume_so_far = max(stock_barset.iloc[:, -1])
+        today_high_so_far = max(stock_barset.iloc[:-1, 2])
+        today_volume_so_far = max(stock_barset.iloc[:-1, -1])
 
         volume_max, high_max = max(max(ticker_data['volume']), today_volume_so_far), max(max(ticker_data['high']), today_high_so_far)
-        if high >= self.alpha_price * high_max and max(volume, volume_moving) >= self.alpha_volume * volume_max and ticker not in self.holding_stocks:
+        if high >= self.alpha_price * high_max and max(volume, volume_moving) >= self.alpha_volume * volume_max:
             try:
                 response = self.create_order(ticker, self.limit_order // current_price, 'buy', 'market', 'gtc')
-                self.holding_stocks.append(ticker)
+                self.holding_stocks.update({ticker: (current_price, self.limit_order // current_price)})
                 logging.warning(response)
             except:
                 logging.warning('Order failed')
@@ -75,10 +91,8 @@ class LiveTrade(object):
         print(f'{ticker}, volume: {volume_max, volume, volume_moving, today_volume_so_far}, price: {high_max, current_price, high, high_moving, today_high_so_far}')
 
     def run(self):
-        self.setup()
-        data = self.load_data()
-        ticker_list = data.keys()
-
+        data, ticker_list = self.setup()
+        self.holding_stocks = self.load_data('holding')
         logging.warning(f'Start @ {datetime.now()}')
 
         for ticker in ticker_list:
@@ -88,6 +102,12 @@ class LiveTrade(object):
                 t.sleep(20)
                 self.find_signal(ticker, data[ticker])
                 pass
+        
+        self.save_data('holding')
+
+    def high_avg_check(self, ticker):
+        if datetime.now().hour <= 15:
+            return True
 
     def create_order(self, symbol, qty, side, order_type, time_in_force):
         data = {
@@ -98,16 +118,12 @@ class LiveTrade(object):
             "time_in_force": time_in_force
         }
 
-        HEADERS = {'APCA-API-KEY-ID': 'PKDLZFEJND51O3Q3TZ3F', 
-                    'APCA-API-SECRET-KEY': 'xguRqEqr3cw0sNA2cQcxIeCuIacqN9mXnvB8LyaO'}
-        BASE_URL = 'https://paper-api.alpaca.markets'
-        ORDERS_URL = "{}/v2/orders".format(BASE_URL)
-
-        r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+        ORDERS_URL = "{}/v2/orders".format(self.base_url)
+        r = requests.post(ORDERS_URL, json=data, headers=self.request_headers)
         return json.loads(r.content)
 
 if __name__ == "__main__":
-    trade = LiveTrade(alpha_price=0.9, alpha_volume=1.2, balance=100000, volatility=10)
+    trade = LiveTrade(alpha_price=0.9, alpha_volume=1.3, balance=100000, volatility=10)
     schedule.every(1).seconds.do(trade.run)
     while True:
         schedule.run_pending()

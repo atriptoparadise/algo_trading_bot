42b3a2ac2ed4edb292720a4c055fec85fff12714
atriptoparadise <qs2178@columbia.edu>
2020-10-29 22:33:02 -0400

    Modified class attributes

--- monitor.py
+++ monitor.py
@@ -15,24 +15,24 @@ eastern = pytz.timezone('US/Eastern')
 
 
 class PortfolioMonitor(LiveTrade):
-    def __init__(self, alpha_price, alpha_volume, balance, volatility, stop_ratio, high_to_current_ratio, current_to_open_ratio, stop_earning_ratio, stop_earning_ratio_high):
-        super().__init__(alpha_price, alpha_volume, balance, volatility, stop_ratio, high_to_current_ratio, current_to_open_ratio, stop_earning_ratio, stop_earning_ratio_high)
+    def __init__(self, alpha_price, alpha_volume, balance, volatility, high_to_current_ratio, current_to_open_ratio):
+        super().__init__(alpha_price, alpha_volume, balance, volatility, high_to_current_ratio, current_to_open_ratio)
         self.closed_orders = []
         self.api = tradeapi.REST(API_KEY, 
                                 SECRET_KEY, 
                                 api_version = 'v2')
 
-    def run(self):
+    def run(self, stop_ratio, stop_earning_ratio, stop_earning_ratio_high):
         positions = self.get_positions()
         self.get_closed_orders()
-        logging.warning(f'Portfolio monitor start @ {datetime.now()}')
+        print(f'Portfolio monitor start @ {datetime.now()}')
 
         for ticker in self.holding_stocks:
             try:
-                self.portfolio_monitor(ticker, positions)
+                self.portfolio_monitor(ticker, positions, stop_ratio, stop_earning_ratio, stop_earning_ratio_high)
             except:
                 t.sleep(20)
-                self.portfolio_monitor(ticker, positions)
+                self.portfolio_monitor(ticker, positions, stop_ratio, stop_earning_ratio, stop_earning_ratio_high)
                 pass
 
     def get_closed_orders(self):
@@ -50,11 +50,11 @@ class PortfolioMonitor(LiveTrade):
             idx += 1
         return stock_barset.iloc[idx:, 2].max()
 
-    def portfolio_monitor(self, ticker, positions):
+    def portfolio_monitor(self, ticker, positions, stop_ratio, stop_earning_ratio, stop_earning_ratio_high):
         data = next(item for item in positions if item['symbol'] == ticker)
         current_price, entry_price, qty = float(data['current_price']), float(data['avg_entry_price']), float(data['qty'])
 
-        if current_price <= self.stop_ratio * entry_price:
+        if current_price <= stop_ratio * entry_price:
             try:
                 self.create_order(symbol=ticker, qty=qty, side='sell', order_type='market', time_in_force='gtc')
                 logging.warning(f'Sold {ticker} at {current_price} v.s. {entry_price} @ {datetime.now()}')
@@ -64,7 +64,7 @@ class PortfolioMonitor(LiveTrade):
                 pass
         
         highest_price = self.get_highest_price(ticker)
-        if highest_price > self.stop_earning_ratio_high * entry_price and (current_price - entry_price) / (highest_price - entry_price) <= self.stop_earning_ratio:
+        if highest_price > stop_earning_ratio_high * entry_price and (current_price - entry_price) / (highest_price - entry_price) <= stop_earning_ratio:
             try:
                 self.create_order(symbol=ticker, qty=qty, side='sell', order_type='market', time_in_force='gtc')
                 logging.warning(f'Sold {ticker} at {current_price} v.s. entry price {entry_price} v.s. highest price {highest_price} @ {datetime.now()}')
@@ -75,9 +75,8 @@ class PortfolioMonitor(LiveTrade):
 
 
 if __name__ == "__main__":
-    monitor = PortfolioMonitor(alpha_price=0.9, alpha_volume=1.3, balance=100000, 
-                        volatility=10, stop_ratio=0.96, high_to_current_ratio=0.2,
-                        current_to_open_ratio=1.15, stop_earning_ratio=0.5, stop_earning_ratio_high=1.07)
-    schedule.every(59).seconds.do(monitor.run)
+    monitor = PortfolioMonitor(alpha_price=0.9, alpha_volume=1.3, balance=8000, 
+                        volatility=8,  high_to_current_ratio=0.2, current_to_open_ratio=1.15)
+    schedule.every(59).seconds.do(monitor.run, stop_ratio=0.95, stop_earning_ratio=0.5, stop_earning_ratio_high=1.07)
     while True:
         schedule.run_pending()
